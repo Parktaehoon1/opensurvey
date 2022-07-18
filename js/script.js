c5dd4f45e3c8623d9c589b7a773d6571c6f5ea6e
Parktaehoon1 <92parkth@gmail.com>
2022-07-18 17:18:59 +0900

    코드진행중

--- js/script.js
+++ js/script.js
@@ -1,8 +1,39 @@
 window.onload = function () {
+	// 안내창
+  // 추가기능 : 스크롤바 없애기
+  $('html').css('overflow', 'hidden');
+  // 
+
+  let modalWrap = $('.modal-wrap');
+  let modalClose = $('.modal-close');
+  
+
+  modalClose.click(function(){
+    modalWrap.stop().fadeOut(500)
+    // 추가기능 : 스크롤바 살리기
+    $('html').css('overflow', 'auto');
+  });
+  let modalMain = $('.madal-main')
+  //내용 배경 클릭
+  modalMain.click(function(event){
+    // 클릭 정보 전달 막기
+    event.stopPropagation();
+  });
+  //전체 배경 클릭
+  modalWrap.click(function(){
+    modalWrap.stop().fadeOut(500)
+    // 추가기능 : 스크롤바 살리기
+    $('html').css('overflow', 'auto');
+  });
+  
+
+
+
 	let platform_desc_list_main_top = $('.platform-desc-list-main-top')
 	let platform_btn_top = $('.platform-btn-top')
 
 	let platform_top_img = $('.platform-top-img')
+
 	let platform_btn_middle = $('.platform-btn-middle')
 	let platform_middle_img_open = $('.platform-middle-img-open')
 	let platform_desc_list_main_middle = $('.platform-desc-list-main-middle')
@@ -11,7 +42,9 @@ window.onload = function () {
 
 	let platform_btn_bottom = $('.platform-btn-bottom')
 	let platform_desc_list_main_bottom = $('.platform-desc-list-main-bottom')
-
+	let platform_desc_list_bottom = $('.platform-desc-list-bottom')
+	
+	
 	// 탑 버튼 클릭
 	platform_btn_top.click(function () {
 		platform_desc_list_main_top.toggleClass('platform-desc-list-main-top-open')
@@ -31,7 +64,7 @@ window.onload = function () {
 		platform_desc_list_main_bottom.toggleClass('platform-desc-list-main-bottom-open')
 		platform_top_img.toggleClass('platform-bottom-img-open')
 		platform_btn_bottom.toggleClass('platform-btn-rotate180');
-		platform_desc_list_main_bottom.toggleClass('platform-desc-list-bg')
+		platform_desc_list_bottom.toggleClass('platform-desc-list-bg')
 	});
 
 
@@ -48,15 +81,31 @@ window.onload = function () {
     },
 	})
 
+
+
+
 	// mb-bt 클릭시 mb-wrap 나오기
 	let mb_bt =$('.mb-bt');
 	let mb_wrap = $('.mb-wrap');
-
+	let mb_bt_open = $('mb-bt-open')
 	mb_bt.click(function(){
-		mb_wrap.toggleClass('mb-wrap-open')		
+		// mb_wrap.toggleClass('mb-wrap-open')		
 		mb_bt.toggleClass('mb-bt-open').fadeIn(500)
+		mb_wrap.stop().animate({
+			left: 0,
+			top:70,
+		}, 1000)
 	});
-	
+	// mb_bt_open.click(function(){
+	// 	mb_wrap.stop().animate({
+	// 		left: 1000,
+	// 		top:70
+	// 	}, 1000)
+
+	// })
+
+
+
 	$(window).resize(function () {
 		let temp = $(window).width();
 		if (temp > 1200) {

4567db9cc19dc70b86311208a1f9e8ad111cffed
Parktaehoon1 <92parkth@gmail.com>
2022-09-05 09:44:09 +0900

    모달창 완료

--- js/script.js
+++ js/script.js
@@ -1,29 +1,29 @@
 window.onload = function () {
-	// 안내창
-  // 추가기능 : 스크롤바 없애기
-  // $('html').css('overflow', 'hidden');
+// 	안내창
+//   추가기능 : 스크롤바 없애기
+  $('html').css('overflow', 'hidden');
 
-  // let modalWrap = $('.modal-wrap');
-  // let modalClose = $('.modal-close');
+  let modalWrap = $('.modal-wrap');
+  let modalClose = $('.modal-close');
   
 
-  // modalClose.click(function(){
-  //   modalWrap.stop().fadeOut(500)
-  //   // 추가기능 : 스크롤바 살리기
-  //   // $('html').css('overflow', 'auto');
-  // });
-  // let modalMain = $('.modal-main')
-  // //내용 배경 클릭
-  // modalMain.click(function(event){
-  //   // 클릭 정보 전달 막기
-  //   event.stopPropagation();
-  // });
-  // //전체 배경 클릭
-  // modalWrap.click(function(){
-  //   modalWrap.stop().fadeOut(500);
-  //   // 추가기능 : 스크롤바 살리기
-  //   // $('html').css('overflow', 'auto');
-  // });
+  modalClose.click(function(){
+    modalWrap.stop().fadeOut(500)
+    // 추가기능 : 스크롤바 살리기
+    // $('html').css('overflow', 'auto');
+  });
+  let modalMain = $('.modal-main')
+  //내용 배경 클릭
+  modalMain.click(function(event){
+    // 클릭 정보 전달 막기
+    event.stopPropagation();
+  });
+  //전체 배경 클릭
+  modalWrap.click(function(){
+    modalWrap.stop().fadeOut(500);
+    // 추가기능 : 스크롤바 살리기
+    // $('html').css('overflow', 'auto');
+  });
   
 
 
